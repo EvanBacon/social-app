1d3c92ff8f4abec966f11219eb847a73c9c45da8
evanbacon <bacon@expo.io>
2023-05-16 10:44:36 -0500

    deployed

--- src/view/screens/PrivacyPolicy.tsx
+++ src/view/screens/PrivacyPolicy.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import {View} from 'react-native'
 import {useFocusEffect} from '@react-navigation/native'
-import {NativeStackScreenProps, CommonNavigatorParams} from 'lib/routes/types'
 import {ViewHeader} from '../com/util/ViewHeader'
 import {useStores} from 'state/index'
 import {ScrollView} from 'view/com/util/Views'
@@ -10,8 +9,7 @@ import {usePalette} from 'lib/hooks/usePalette'
 import {s} from 'lib/styles'
 import PrivacyPolicyHtml from '../../locale/en/privacy-policy'
 
-type Props = NativeStackScreenProps<CommonNavigatorParams, 'PrivacyPolicy'>
-export const PrivacyPolicyScreen = (_props: Props) => {
+export const PrivacyPolicyScreen = () => {
   const pal = usePalette('default')
   const store = useStores()
 
